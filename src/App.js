e67b851ccc9fa6a4c3999ec10a98b0633a241542
Sparsh Goel <33709657+officialcody@users.noreply.github.com>
2023-11-04 17:54:16 +0000

    Added dynamic multiplier functionaity

--- src/App.js
+++ src/App.js
@@ -2,16 +2,23 @@ import { useState } from "react";
 
 const App = () => {
     const [counter, setCounter] = useState(0);
+    const [fieldValue, setFieldValue] = useState(5);
     const incrementCounter = () => {
-        setCounter(counter + 1);
+        setCounter(counter + (1 * fieldValue));
     }
 
     const decrementCounter = () => {
-        setCounter(counter - 1);
+        setCounter(counter - (1 * fieldValue));
     }
 
     return (<div>
         <h1>{counter}</h1>
+        <input
+            type="number"
+            value={fieldValue}
+            placeholder="Pick a multiplier for increment and decrement"
+            onChange={e => setFieldValue(e.target.value)}
+        />
         <button onClick={incrementCounter}>
             Increment
         </button>
