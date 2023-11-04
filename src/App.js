11bcdc5338ce5df23bf4081482bfd02f5a1fc733
Sparsh Goel <33709657+officialcody@users.noreply.github.com>
2023-11-04 18:17:54 +0000

    Update the code to fulfill the edge case where field value is null

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 const App = () => {
     const [counter, setCounter] = useState(0);
-    const [fieldValue, setFieldValue] = useState(5);
+    const [fieldValue, setFieldValue] = useState(0);
+    const inputRef = useRef(fieldValue);
+
+    let multiplier = inputRef.current.value === '' || inputRef.current.value === '0' ? 1 : fieldValue;
+    
     const incrementCounter = () => {
-        setCounter(counter + (1 * fieldValue));
+        setCounter(counter + (1 * multiplier));
     }
 
     const decrementCounter = () => {
-        setCounter(counter - (1 * fieldValue));
+        setCounter(counter - (1 * multiplier));
     }
 
     return (<div>
         <h1>{counter}</h1>
         <input
+            ref={inputRef}
             type="number"
             value={fieldValue}
             placeholder="Pick a multiplier for increment and decrement"
